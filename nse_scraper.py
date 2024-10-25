d586c6ef03f74be3db47dd44684a39a45c3d8ff5
rakesh9907 <161925907+rakesh9907@users.noreply.github.com>
2024-10-25 12:44:04 +0000

    fixed scraper

--- nse_scraper.py
+++ nse_scraper.py
@@ -4,29 +4,36 @@ import requests
 nse_url = 'https://www.nseindia.com'
 
 # Headers to mimic a real browser request
-headers = {
+head = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
   'Accept-Encoding': 'gzip, deflate, br',
   'Accept-Language': 'en-US,en;q=0.5',
   'Connection': 'keep-alive',
-  'Upgrade-Insecure-Requests': '1'
+  # 'Upgrade-Insecure-Requests': '1',
+  # 'Referer': 'https://www.nseindia.com/report-detail/eq_security',  # Adding Referer header
+  # 'X-Requested-With': 'XMLHttpRequest'  # This may be required for AJAX requests
 }
 
 def fetch_nse_data():
   session = requests.Session()
-  response = session.get(nse_url, headers=headers)
+  response = session.get(nse_url, headers=head)
 
   if response.status_code == 200:
     print("Successfully retrieved response from NSE")
-    return session
+    cookies = response.cookies
+    header = response.headers
+    return [session, cookies, header]
   else:
     print(f"Failed to retrieve response from NSE. Status code: {response.status_code}")
 
 def fetch_delivery(symbol, start_date, end_date):
-  session = fetch_nse_data()
-  target_url = f"https://www.nseindia.com/api/historical/securityArchives?from={start_date.strip('\"')}&to={end_date.strip('\"')}&symbol={symbol}&dataType=priceVolumeDeliverable&series=ALL"
-  print(target_url)
-  response = session.get(target_url, headers=headers)
+  session, cookies, header = fetch_nse_data()
+  target_url = f'https://www.nseindia.com/api/historical/securityArchives?from={start_date.strip("'\" ")}&to={end_date.strip("'\" ")}&symbol={symbol}&dataType=priceVolumeDeliverable&series=ALL'
+  response = session.get(target_url, headers=head, cookies=cookies)
   data = response.json()
   return data
+
+
+
+# nsetool in pack use for get data 
