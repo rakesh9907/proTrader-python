69bca52623332407f2eb5d01f9c21e7624545355
rakesh9907 <161925907+rakesh9907@users.noreply.github.com>
2024-10-25 14:44:07 +0000

    fixed header

--- nse_scraper.py
+++ nse_scraper.py
@@ -6,10 +6,10 @@ nse_url = 'https://www.nseindia.com'
 # Headers to mimic a real browser request
 head = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-  'Accept-Encoding': 'gzip, deflate, br',
-  'Accept-Language': 'en-US,en;q=0.5',
-  'Connection': 'keep-alive',
+  # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+  # 'Accept-Encoding': 'gzip, deflate, br',
+  # 'Accept-Language': 'en-US,en;q=0.5',
+  # 'Connection': 'keep-alive',
   # 'Upgrade-Insecure-Requests': '1',
   # 'Referer': 'https://www.nseindia.com/report-detail/eq_security',  # Adding Referer header
   # 'X-Requested-With': 'XMLHttpRequest'  # This may be required for AJAX requests
